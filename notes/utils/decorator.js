c64932a97d99acc36fcab4d0944303120f9bd40d
texnolyze <wiskasers@mail.ru>
2026-03-12 11:48:35 +0300

    Update decorator.js

--- notes/utils/decorator.js
+++ notes/utils/decorator.js
@@ -1,1 +1,13 @@
+class Decorator{
+  static drawLine(){
+    console.log("=".repeat(30));
+  }
 
+  static presentMenu(welcome){
+    drawLine();
+    console.log('\n');
+    console.log(`${welcome}`);
+    drawLine();
+  }
+}
+module.exports = Decorator;
