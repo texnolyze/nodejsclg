f969e5eea933f6d84a27ec947fc23fe0c7dad10c
texnolyze <wiskasers@mail.ru>
2026-03-12 10:45:18 +0300

    Update helper.js

--- notes/utils/helper.js
+++ notes/utils/helper.js
@@ -1,1 +1,6 @@
-// Скрипт помошник с дополнительными функциями для index.js
+// Скрипт помошник с дополнительными функциями для index.js (note.js)
+const reindexId = (notes) => {
+  return notes.map((notes, index) => ({...notes, id:index + 1}));
+};
+
+module.export = {reindexId};
