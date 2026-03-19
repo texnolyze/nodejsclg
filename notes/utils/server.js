06e5e559571703e99d6f9fb1e5017cc58bdb24de
texnolyze <wiskasers@mail.ru>
2026-03-19 10:28:00 +0300

    Update server.js

--- notes/utils/server.js
+++ notes/utils/server.js
@@ -9,7 +9,18 @@ let notes = fileManager.loadFile();
 
 const server = http.createServer(async (req, res) => {
   const {url, method} = req;  
-});
+
+  if(url === '/' && method === 'GET'){
+    const html = await fs.readFile(path.join(__dirname, 'index.html'), 'utf-8');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
+  }
+  
+ if(url === '/app.js' && method === 'GET'){
+   const html = await fs.readFile(path.join(__dirname, 'app.js'), 'utf-8');
+   res.writeHead(200, {'Content-Type': 'application/javascript'});
+   res.end(js);
+}
 
 server.listen(3000, () => {
   console.log("Сервер запущен на порту http://localhost:3000");
