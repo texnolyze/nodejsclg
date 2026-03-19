fe456b1c4b4be36669cde1330c21b9c84989428e
texnolyze <wiskasers@mail.ru>
2026-03-19 09:28:16 +0300

    Update server.js

--- notes/utils/server.js
+++ notes/utils/server.js
@@ -1,1 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
+const helper = require("./utils/helper");
+const fileManager = require("./utils/fileManager");
+
+let notes = fileManager.loadFile();
+
+const server = http.createServer(async (req, res) => {
+  const {url, method} = req;  
+});
+
+server.listen(3000, () => {
+  console.log("Сервер запущен на порту 3000");
+});
