2a91d4b04b8d17da4805a7cb4c640b626c972f39
texnolyze <wiskasers@mail.ru>
2026-03-19 12:01:22 +0300

    Update app.js

--- notes/app.js
+++ notes/app.js
@@ -5,14 +5,32 @@ let notes = []
 
 async function loadNotes(){
   try{
-   const res = await fetch('/api/notes/');
+    const res = await fetch('/api/notes/');
     notes = await res.json();
-    stats.innerText = `Заметок ${notes.length}` 
+    stats.innerText = `Заметок ${notes.length}`
   }
   catch(error){
-    console.log("Ошибка", error);
-    stats.innerText = `Информации о заметках нет` 
+    console.log("Ощибка", error);
+    stats.innerText = `Информации о заметках нет`
+  }
+}
+
+async function addNote(){
+  const title = prompt("Введите название ");
+  const content = prompt("Введите содержание ");
+
+  try{
+    await fetch('api/notes', {
+      method: "POST",
+      headers: {'Content-Type' : 'application/json' },
+      body: JSON.stringify({title, content})
+    });
+  }
+  catch(error)
+  {
+    console.log("ERROR", error.message)
   }
 }
 
 loadNotes();
+
