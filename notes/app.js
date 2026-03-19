bae47616f407f37a293dee391835830f9c3ae81f
texnolyze <wiskasers@mail.ru>
2026-03-19 10:12:40 +0300

    Update app.js

--- notes/app.js
+++ notes/app.js
@@ -1,1 +1,16 @@
+const stats = document.getElementById('stats');
+const content = document.getElementById('content');
 
+let notes = []
+
+async function loadNotes(){
+  try{
+   const res = await fetch('/api/notes/');
+    notes = await res.json();
+    stats.innerText = `Заметок ${notes.length}` 
+  }
+  catch(error){
+    console.log("Ошибка", error);
+    stats.innerText = `Информации о заметках нет` 
+  }
+}
