50128ddfb5eaedd2c6437f49af8b248ec8348118
texnolyze <wiskasers@mail.ru>
2026-03-19 10:42:33 +0300

    Update server.js

--- notes/utils/server.js
+++ notes/utils/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const helper = require("./utils/helper");
@@ -8,19 +8,31 @@ const fileManager = require("./utils/fileManager");
 let notes = fileManager.loadFile();
 
 const server = http.createServer(async (req, res) => {
-  const {url, method} = req;  
+  const { url, method } = req;
+
+  // ROOT ROUTERS
 
   if(url === '/' && method === 'GET'){
     const html = await fs.readFile(path.join(__dirname, 'index.html'), 'utf-8');
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(html);
   }
+
+  if(url === '/app.js' && method === 'GET'){
+    const js = await fs.readFile(path.join(__dirname, 'app.js'), 'utf-8');
+    res.writeHead(200, {'Content-Type': 'application/javascript'});
+    res.end(js);
+  }
+
+  // API ROUTERS
+
+  if(url === 'api/notes' && method === 'GET'){
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(notes)); 
+  }
+
   
- if(url === '/app.js' && method === 'GET'){
-   const html = await fs.readFile(path.join(__dirname, 'app.js'), 'utf-8');
-   res.writeHead(200, {'Content-Type': 'application/javascript'});
-   res.end(js);
-}
+});
 
 server.listen(3000, () => {
   console.log("Сервер запущен на порту http://localhost:3000");
